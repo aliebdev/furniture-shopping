9aeb5a529b360bd9f689bd91f7570d961732d264
Ali Ebadi <ebadialideveloper@gmail.com>
2022-07-26 00:09:07 +0430

    update home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../models/item_model.dart';
 import '../theme/app_spacings.dart';
 import '../theme/colors.dart';
 import 'widgets/category_widget.dart';
@@ -36,6 +37,88 @@ class HomeScreen extends StatelessWidget {
                 const _HeaderText(),
                 const SizedBox(height: AppSpacings.xxxl),
                 const CategoryWidget(),
+                const SizedBox(height: AppSpacings.xxxl),
+                Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(AppSpacings.xl),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: AppSpacings.xl,
+                      crossAxisSpacing: AppSpacings.xl,
+                      childAspectRatio: .7,
+                    ),
+                    itemCount: ItemModel.items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = ItemModel.items[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.shade300,
+                              blurRadius: 10,
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(AppSpacings.l),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              flex: 6,
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(AppSpacings.l),
+                                  topRight: Radius.circular(AppSpacings.l),
+                                ),
+                                child: Image.asset(
+                                  item.image,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Container(
+                                padding: const EdgeInsets.all(AppSpacings.l),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        item.title,
+                                        textAlign: TextAlign.center,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium!
+                                            .copyWith(
+                                              color: AppColors.black,
+                                              fontWeight: FontWeight.bold,
+                                              overflow: TextOverflow.ellipsis,
+                                              fontSize: 15,
+                                            ),
+                                      ),
+                                    ),
+                                    Text(
+                                      item.price,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleSmall!
+                                          .copyWith(
+                                            color: AppColors.red,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                )
               ],
             ),
           )
