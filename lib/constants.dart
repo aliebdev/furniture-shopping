10788e24694107acb32f8e515e41255a83c41bba
Ali Ebadi <ebadialideveloper@gmail.com>
2022-07-25 22:19:45 +0430

    update constants & assets

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,3 +12,22 @@ final List<String> categories = [
   "Storage",
   "Bar Furniture",
 ];
+
+final List<Map<String, String>> bottomAppbarItems = [
+  {
+    "selected": "assets/icons/home_selected.svg",
+    "unSelected": "assets/icons/home_unselected.svg",
+  },
+  {
+    "selected": "assets/icons/favorite_selected.svg",
+    "unSelected": "assets/icons/favorite_unselected.svg",
+  },
+  {
+    "selected": "assets/icons/cart_selected.svg",
+    "unSelected": "assets/icons/cart_unselected.svg",
+  },
+  {
+    "selected": "assets/icons/profile_selected.svg",
+    "unSelected": "assets/icons/profile_unselected.svg",
+  },
+];
