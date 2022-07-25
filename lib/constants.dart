0cc114dd3b53222dbf22cd647d59495417deef84
Ali Ebadi <ebadialideveloper@gmail.com>
2022-07-26 00:12:06 +0430

    update models

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,3 @@
-final List<String> items = [
-  "assets/images/1.jpeg",
-  "assets/images/2.jpeg",
-  "assets/images/3.jpeg",
-  "assets/images/4.jpeg",
-];
-
 final List<String> categories = [
   "Chairs",
   "Sofas",
